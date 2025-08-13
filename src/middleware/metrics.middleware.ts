d7d0d3859090ec7ce09656dc546695444155cf7f
Shubham Sharma <shubhamsharma10403@gmail.com>
2025-08-13 10:44:19 +0530

    added Gauge  metric

--- src/middleware/metrics.middleware.ts
+++ src/middleware/metrics.middleware.ts
@@ -1,18 +1,28 @@
 import { Response, Request, NextFunction } from "express";
 import { requestCounter } from "../metrics/requestCount";
-
+import { activeRequestsGauge } from "../metrics/activeRequestsGauge";
 export function metricsMiddleware(req: Request, res: Response, next: NextFunction) {
     const start = Date.now();
-     
+   
+    if(req.path !== "/metrics"){
+        activeRequestsGauge.inc();                    // Increment the active requests gauge
+    }
+
     res.on('finish', () => {                         // 'finish' event is emitted when the response has been sent
         const duration = Date.now() - start;
         console.log(`Request took ${duration} ms`);
         
-        requestCounter.inc({                        // Increment the request counter
+        requestCounter.inc({                         // Increment the request counter
             method: req.method,
             route: req.originalUrl,
             status_code: res.statusCode
         });
+
+        if(req.path !== "/metrics"){
+            activeRequestsGauge.dec();              // Decrement the active requests gauge
+        }
+
     });
+    
     next();
 }
